14e7e69902ed86db718111fb3e4eb86f6d9267f7
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-05-10 20:54:11 +0530

    db connected

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ require('dotenv').config();
 const DB_URL = process.env.DB_CONNECTION_URL;
 const e = require('express');
 const express = require('express');
+const mongoose = require('mongoose');
+var port = process.env.PORT || 3001;
+const DB_CONNECTION_OPTIONS = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+mongoose.connect(DB_URL, DB_CONNECTION_OPTIONS).then(
+    res=>{
+        console.log("connected to db");
+        app.listen(port, () => {
+            console.log("listening at port-" + port);
+        })
+        
+    }
+).catch(
+    err=>{
+        console.error("db connection failed"+err);
+    }
+);
 const app = express();
 
 app.get('/', (req, responseObject) => {
     responseObject.send({ "message": "hello" });
 })
 
-var port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log("listening at port" + port);
-})
+
 
