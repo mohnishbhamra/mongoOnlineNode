0861db64045e74a683b54b4a1c713399960b2fe7
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-05-10 21:42:33 +0530

    using personModel

--- routes/person.js
+++ routes/person.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const PersonModel = require('../models/personModel');
 
 router.get('/',(req,res)=>{
     var msg = "we are on person router";
+    console.log(msg);
+    res.send(msg);
+})
+
+
+router.post('/',(req,res)=>{
+    var msg = "we are on person router post"+req.body;
     console.log(msg);
     res.send(msg);
 })
